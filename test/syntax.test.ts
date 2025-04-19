3d956ccfd819813566b2e7abe31fe44cfeb5c60c
sjegink <sjegink@gmail.com>
2025-04-19 14:50:11 +0900

    feat: Test with vitest not jest

--- test/syntax.test.ts
+++ test/syntax.test.ts
@@ -1,19 +1,20 @@
-import { syntax as korUtilSyntax } from '../index';
+import { expect, test } from 'vitest';
+import korUtil from '../lib/kor-util';
 
 test('fixPostPositions()', () => {
 
-	expect(korUtilSyntax.fixPostPositions(
+	expect(korUtil.fixPostPositions(
 		`${'길동'}은(는) ${'어른'}(이)고, ${'희동'}(이)는 ${'어린이'}(이)다.`
 	)).toBe(
 		`길동은 어른이고, 희동이는 어린이다.`
 	);
 
-	expect(korUtilSyntax.fixPostPositions(`하늘가(이) 땅이(가)`
+	expect(korUtil.fixPostPositions(`하늘가(이) 땅이(가)`
 	)).toBe(
 		`하늘가 땅이`
 	);
 
-	expect(korUtilSyntax.fixPostPositions(`${'확'}율(률) ${'할인'}률(율)`
+	expect(korUtil.fixPostPositions(`${'확'}율(률) ${'할인'}률(율)`
 	)).toBe(
 		`확률 할인율`
 	);
